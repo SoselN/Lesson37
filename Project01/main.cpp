8347457ab645a4b5159a4cadd45163ae812f598b
SoselN <158481488+SoselN@users.noreply.github.com>
2024-02-01 18:41:29 +0300

    Update main.cpp

--- Project01/main.cpp
+++ Project01/main.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
-int sum(int num1, int num2);
+int sum(int num1, int num2, int nim3);
 
 int main() {
 
 	int num1 = 5;	
 	int num2 = 9;
+	int num3 = 7;
 	
-	cout << sum(num1, num2) << endl;
+	cout << sum(num1, num2, num3) << endl;
 	return 0;
 }
